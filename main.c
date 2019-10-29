b450adc213693575eee389031109d8480f475f5e
im-zero57 <leman000@sookmyung.ac.kr>
2019-10-29 22:13:47 +0900

    실습 1_function ver

--- main.c
+++ main.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(void) {
+void print_star()
+{
 	int i;
+	for(i=0;i<10;i++)
+		printf("*");
+}
+int main(void) {
 	
-	for(i=0;i<10;i++)
-		printf("*");
-	for(i=0;i<10;i++)
-		printf("*");
-	for(i=0;i<10;i++)
-		printf("*");
+	print_star();
+	print_star();
+	print_star();
 				
 	return 0;
 }
