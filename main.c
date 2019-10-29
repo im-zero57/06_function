33041f07f15767fee0597453fa62c8bfaa33c477
im-zero57 <leman000@sookmyung.ac.kr>
2019-10-29 22:47:25 +0900

    실습  7

--- main.c
+++ main.c
@@ -1,18 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int factorial(int n)
+{
+	int res = 1;
+	int i;
+	
+	for(i=1;i<=n;i++)
+		res=res*i;
+	
+	return res;	   
+}
+
+int combination(int n, int r)
+{
+	int high, low;
+	high = factorial(n);
+	low = (factorial(n-r))*(factorial(r));
+	return(high/low);
+}
 
 int main(void)
 {
-	int i;
-	int max = 45;	
+	int n,r;
+	int result;
 	
-	srand((unsigned)time(NULL));
-	for(i=0;i<6;i++)
-		printf("%d",1+rand()%max);
+	printf("input two integer : ");
+	scanf("%d,%d",&n,&r);
 	
-	return 0;	
+	result=combination(n,r);
+	printf("the result of combination two integer is %d",result);
+	
+	return 0;
 }
 
