715f007dbf53ef3d3e422f7bce667bd7e52bd658
im-zero57 <leman000@sookmyung.ac.kr>
2019-10-29 22:28:16 +0900

    실습 4_인수와 매개변수

--- main.c
+++ main.c
@@ -3,38 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int sumTwo(int a,int b)
+void square (int a)
 {
-	int output;
-	output=a+b;
-	return output;
+	a=a*a;
 }
 
-int square(int n)
-{
-	return(n*n);
-}
-
-int get_max(int a,int b)
-{
-	if(a>b)
-		return (a);
-	else
-		return (b);	
-}
 
 int main() {
-	int a,b;
-	a=2;
-	b=5;
 	
-	int result_sumTwo;
-	result_sumTwo = sumTwo(a,b);
+	int a = 2;
+	square(a);
+	printf("a=%i\n",a);
 	
-	printf("the result of sum two number is %d\n",result_sumTwo);
-	printf("the result of square 2 is %d\n",square(a));
-	printf("the result of square 5 is %d\n",square(b));
-	printf("the result of larger number 2 and 5 is %d\n",get_max(a,b));
-				
-	return 0;
+return 0;
 }
